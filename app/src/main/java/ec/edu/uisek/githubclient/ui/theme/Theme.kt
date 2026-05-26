f1ac11389a9968ff271310665e5c4bb605fc74b7
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2026-05-26 14:24:19 -0500

    Soporta método POST

--- app/src/main/java/ec/edu/uisek/githubclient/ui/theme/Theme.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/ui/theme/Theme.kt
@@ -9,18 +9,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import ec.edu.uisek.githubclient.ui.theme.UiSEKBlue
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = UiSEKBlue,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    primaryContainer = UiSEKBlue,
+    onPrimaryContainer = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = UiSEKBlue,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    primaryContainer = UiSEKBlue,
+    onPrimaryContainer = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +43,7 @@ private val LightColorScheme = lightColorScheme(
 fun GithubClientTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
